5388f1b921ba82638d003651cbf91aa5f70a5345
Amit Lakade <99075733+lakadeamit220@users.noreply.github.com>
2025-03-05 18:15:18 +0530

    implemented string functionalities of redis

--- string.js
+++ string.js
@@ -3,9 +3,14 @@ const client = require('./client');
 async function init() {
   const result = await client.get('user:3');
   console.log("User_3: ", result);
+
   await client.set('msg:3', 'I am working on YuKa Project!!');
   const msg3 = await client.get('msg:3');
   console.log("msg_3: ", msg3);
+
+  await client.expire('msg:3', 10);
+  const msg4 = await client.get('msg:3');
+  console.log("msg_4: ", msg4);
 }
 
 init()
